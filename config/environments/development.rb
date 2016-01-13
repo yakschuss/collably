563e749c26890cb0c7e5f8225793452b1b1c7812
Jack Schuss <jackschuss@gmail.com>
2016-01-13 10:39:28 -0500

    hook up mailtrap.io

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,4 +40,15 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :user_name => '52760ce832edac3e6',
+  :password => '409a626e784248',
+  :address => 'mailtrap.io',
+  :domain => 'mailtrap.io',
+  :port => '2525',
+  :authentication => :cram_md5
+}
 end
