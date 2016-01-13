38e7f9f247ec18fb76370202b73b075092c97d28
Jack Schuss <jackschuss@gmail.com>
2016-01-13 14:47:30 -0500

    add first_name/last_name to user, edit registration views to allow attributes persistence

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160112222807) do
+ActiveRecord::Schema.define(version: 20160113194119) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 20160112222807) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
