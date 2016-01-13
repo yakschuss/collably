478a4baddea4e58a0ed7198a2d067fb83e812822
Jack Schuss <jackschuss@gmail.com>
2016-01-13 14:39:52 -0500

    update user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,13 @@
-
+FactoryGirl.define do
+  sequence :email do |n|
+    "person#{n}@example.com"
+  end
+end
 
 FactoryGirl.define do
-
-  factory :user do
-
+  factory :user, class: 'User' do
     email
-    password
-    password_confirmation
-    
+    password 'password'
+    password_confirmation 'password'
   end
 end
