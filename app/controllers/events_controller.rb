70c730edec025e8d8e198601715e29237eaa26c3
Jack Schuss <jackschuss@gmail.com>
2016-01-14 12:50:05 -0500

    stub out events_controller actions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,13 @@
 class EventsController < ApplicationController
-  def index
+  before_action: :authenticate_user!, only: [:new, :create, :show]
+
+  def new
   end
+
+  def create
+  end
+
+  def show
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'events/new'
+
+  get 'events/create'
+
+  get 'events/show'
+
   devise_for :users, controllers: {registrations: 'registrations'}
   resources :events
   root to: 'welcome#index'
