8a9154801e4519aa3c2c508b9b1f15093acfb9e6
Jack Schuss <jackschuss@gmail.com>
2016-01-14 16:24:17 -0500

    add event#show view, edit events#controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,13 +1,18 @@
 class EventsController < ApplicationController
-  before_action: :authenticate_user!, only: [:new, :create, :show]
+  
+  before_action :authenticate_user!, only: [:new, :create, :show]
 
   def new
+    @event = Event.new
   end
 
   def create
+    @event = Event.create(params[:title])
+    @event.user = current_user
   end
 
   def show
+    @event = Event.find(params[:id])
   end
 
 end
