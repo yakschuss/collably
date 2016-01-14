3ac258aae23f773b76da2d72c7cd8ce0501cf6e1
Jack Schuss <jackschuss@gmail.com>
2016-01-14 16:30:52 -0500

    added user controller, user#show view

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
 
-  get 'events/new'
-
-  get 'events/create'
-
-  get 'events/show'
 
   devise_for :users, controllers: {registrations: 'registrations'}
   resources :events
+  resources :users, only: :show
   root to: 'welcome#index'
 end
