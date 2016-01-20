c187d9c1908af68cfe02eaab4199af0ec4e22348
Jack Schuss <jackschuss@gmail.com>
2016-01-20 18:35:03 -0500

    add user role to event#show

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,13 @@ class User < ActiveRecord::Base
     has_many :roles, class_name: "EventUserRole"
     has_many :events, through: :roles
 
+
+    def role(event_id)
+      query = EventUserRole.where(event_id: event_id, user_id: self.id).take
+      query ? query.role : "none"
+    end
+
+    def full_name
+      return "#{self.first_name} #{self.last_name}"
+    end
 end
