509c8b530b53faa3a16e90c04d6184e5c28b7837
Jack Schuss <jackschuss@gmail.com>
2016-01-22 16:05:41 -0500

    add invite_user method - (need to handle to_s bug)

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,11 +4,23 @@ class Event < ActiveRecord::Base
 
   validates :title, length: {minimum: 5}, presence: true
 
-#  scope :administrators, -> {where(roles: :admin)}
-#  scope :members, -> {where(roles: :member)}
+  #scope :admin, -> { where(roles: 'admin') }
+  #scope :members, -> {where(roles: 'member')}
 
-#  def administrators
-#    where
-#  end
+
+
+
+def invite_user(user_email)
+    email = user_email #.to_s
+    invited_user = User.find_by(email: "#{email}")
+    if invited_user
+      self.users << invited_user
+      user =  EventUserRole.find_by(user_id: "#{invited_user.id}", event_id: self.id)
+      user.role ||= :invited
+      user.invited_at = Time.now
+    else
+      #devise_invitable
+    end
+end
 
 end
