1ef41cc490580ef817aceea27aba912a1c30bd96
Jack Schuss <jackschuss@gmail.com>
2016-02-01 11:28:51 -0500

    user#show rerouting bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: [:show]
+#   before_action :authenticate_user!, only: [:show]
   def show
-    @user = current_user
+    @user = User.find(params[:id])
   end
 end
