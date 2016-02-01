ff9c7913f1dd9ffa601ea633cdd71b73047ba293
Jack Schuss <jackschuss@gmail.com>
2016-02-01 13:31:37 -0500

    stub accept/decline actions// model method// view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,30 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def decline_invite
+      @user = User.find(params[:id])
+      @event = Event.find(params[:id])
+
+      if @user.turn_down_invite(@event)
+        flash[:notice] = "Invite Declined"
+        redirect_to @user
+      else
+        flash[:error] = "Unable to process - please try again"
+        render :show
+      end
+  end
+
+  def accept_invite
+    @user = User.find(params[:id])
+    @event = Event.find(params[:id])
+    if @user.accept_the_invite(@event)
+      flash[:notice] = "Welcome!"
+      redirect_to event_path(@event.id)
+    else
+      flash[:error] = "Oops! That didn't work. Please try again."
+      render :show
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,35 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
     has_many :roles, class_name: "EventUserRole"
-    has_many :events, through: :roles
-
+    has_many :events, through: :roles#, conditions: {status: 'accepted'}
+  #  has_many :pending_events, through: :event_user_roles, source: :event#, conditions: {status: 'pending'} accepts_nested_attributes_for :events
 
     def role(event_id)
       query = EventUserRole.where(event_id: event_id, user_id: self.id).take
       query ? query.role : "none"
     end
 
+    def status(event_id)
+      query = EventUserRole.where(event_id: event_id, user_id: self.id).take
+      query ? query.status : "none"
+    end
+
     def full_name
       return "#{self.first_name} #{self.last_name}"
+    end
+
+    def turn_down_invite(event_id)
+      invitation = EventUserRole.where(event_id: event_id, user_id: self.id).take
+        invitation ? invitation.delete : "no such event"
+    end
+
+    def accept_the_invite(event_id)
+      invitation = EventUserRole.where(event_id: event_id, user_id: self.id).take
+      if invitation
+       invitation.status = "accepted"
+       invitation.role = "member"
+      else
+       "no such event"
+      end
     end
 end
