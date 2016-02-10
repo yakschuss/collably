bf860b833c07b337ed729c6b2ddf5c6097da8845
Jack Schuss <jackschuss@gmail.com>
2016-02-10 12:29:32 -0500

    allow facebook signup user to skip devise confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,11 +12,11 @@ class User < ActiveRecord::Base
         user.provider = auth.provider
         user.uid = auth.uid
         user.email = auth.info.email
-      #  user.first_name = auth.info.first_name
-      #  user.last_name = auth.info.last_name
+        user.first_name = auth.info.first_name
+        user.last_name = auth.info.last_name
         user.password = Devise.friendly_token[0,20]
         user.skip_confirmation!
-      #  user.save!
+        user.save!
       end
   end
 
