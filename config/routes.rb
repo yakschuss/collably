3fd58063220aa6dc1f971050f516574643fc248a
Jack <jackschuss@gmail.com>
2016-02-11 12:42:30 -0500

    Revert "Facebook auth"

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: {registrations: 'registrations', omniauth_callbacks: "callbacks"}
-
+  devise_for :users, controllers: {registrations: 'registrations'}
+  
   root to: 'welcome#index'
 end
