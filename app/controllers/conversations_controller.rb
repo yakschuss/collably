68288ecd4725aee69418e36f4e13bdbd01316015
Jack Schuss <jackschuss@gmail.com>
2016-02-13 20:52:38 -0500

    trash and untrash options in inbox

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -21,6 +21,16 @@ class ConversationsController < ApplicationController
     redirect_to conversation_path(conversation)
   end
 
+  def trash
+    conversation.move_to_trash(current_user)
+    redirect_to mailbox_inbox_path
+  end
+
+  def untrash
+    conversation.untrash(current_user)
+    redirect_to mailbox_inbox_path
+  end
+
   private
 
   def conversation_params
