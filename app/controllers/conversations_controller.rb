64f0cdd292fc740754e6997dfd56e8c3834cfc05
Jack Schuss <jackschuss@gmail.com>
2016-02-13 20:24:18 -0500

    write conversations#create, #show

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,4 +2,22 @@ class ConversationsController < ApplicationController
   before_action :authenticate_user!
   def new
   end
+
+  def create
+    recipients = User.where(id: conversation_params[:recipients])
+    conversation = current_user.send_message(recipients, conversation_params[:body], conversation_params[:subject]).conversation
+    flash[:success] = "Your message was sent successfully!"
+    redirect_to conversation_path(conversation)
+  end
+
+  def show
+    @receipts = conversation.receipts_for(current_user)
+    conversation.mark_as_read(current_user)
+  end
+
+  private
+
+  def conversation_params
+    params.require(:conversation).permit(:subject, :body, recipients: [])
+  end
 end
