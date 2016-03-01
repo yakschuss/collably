668190a20df0f1cce81361f7b6f4a600b0d067f2
Jack Schuss <jackschuss@gmail.com>
2016-02-29 21:38:57 -0500

    adjusted oAuth controller

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -286,7 +286,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, "551091951724564", "21b0f4dc04cab177018d02ffa05415f0"
+  config.omniauth :facebook, ENV["OAUTH_FB_ID"], ENV["OAUTH_FB_SECRET"]
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
